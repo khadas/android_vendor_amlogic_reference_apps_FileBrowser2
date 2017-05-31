b6cca89dde5b8677f808f68290b1d4f6fa00f282
Xindong Xu <xindong.xu@amlogic.com>
2017-05-31 14:17:33 +0800

    PD#145255: generate amlogic files to vendor/
    Change-Id: Ia05ad4ca3e883695bafb1455413847be93557185

--- Android.mk
+++ Android.mk
@@ -5,6 +5,10 @@ LOCAL_MODULE_TAGS := optional
 
 LOCAL_SRC_FILES := $(call all-subdir-java-files)
 
+ifeq ($(shell test $(PLATFORM_SDK_VERSION) -ge 26 && echo OK),OK)
+LOCAL_PROPRIETARY_MODULE := true
+endif
+
 LOCAL_PACKAGE_NAME := FileBrowser
 LOCAL_CERTIFICATE := platform
 
