a0772787d9ddcb89e1cf57f2fed77478f24464db
Lei Qian <lei.qian@amlogic.com>
2018-10-19 10:50:15 +0800

    Android.mk: adapte the situation that not firstapi [12/12]
    PD#SWPL-773
    
    Problem:
    TV can't work normally when open firstapi
    
    Solution:
    adapte the situation that not firstapi
    
    Verify:
    verify by p321
    
    Change-Id: I6e46a5ff2f33446b1b0c46c8ca44f1f1c6f6c479
    Signed-off-by: Lei Qian <lei.qian@amlogic.com>

--- Android.mk
+++ Android.mk
@@ -9,7 +9,9 @@ ifeq ($(shell test $(PLATFORM_SDK_VERSION) -ge 26 && echo OK),OK)
 LOCAL_PROPRIETARY_MODULE := true
 endif
 
-#LOCAL_PRIVATE_PLATFORM_APIS := true
+ifndef PRODUCT_SHIPPING_API_LEVEL
+LOCAL_PRIVATE_PLATFORM_APIS := true
+endif
 
 LOCAL_PACKAGE_NAME := FileBrowser
 LOCAL_CERTIFICATE := platform
